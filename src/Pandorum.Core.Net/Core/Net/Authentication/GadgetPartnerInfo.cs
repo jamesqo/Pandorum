a815a6c611bd7559bef78cdbe4973ec93c39b6cb
James Ko <jamesqko@gmail.com>
2016-06-19 00:20:06 -0400

    More work

---
+++ src/Pandorum.Core.Net/Core/Net/Authentication/GadgetPartnerInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pandorum.Core.Net.Authentication
+{
+    internal class GadgetPartnerInfo : IPartnerInfo
+    {
+        public string DecryptPassword => "E#IO$MYZOAB%FVR2";
+        public string DeviceId => "WG01";
+        public string EncryptPassword => "%22CML*ZU$8YXP[1";
+        public string Password => "EVCCIBGS9AOJTSYMNNFUML07VLH8JYP0";
+        public string Username => "windowsgadget";
+    }
+}
---
+++ src/Pandorum/Options/PandoraClientOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pandorum.Options
+{
+    public class PandoraClientOptions
+    {
+        private static PandoraClientOptions s_default;
+
+        internal static PandoraClientOptions Default =>
+            s_default ?? (s_default = new PandoraClientOptions());
+    }
+}
