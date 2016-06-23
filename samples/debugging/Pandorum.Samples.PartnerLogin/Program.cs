c552174c64e5ad99ae368e075c6fb8c12a0d6047
James Ko <jamesqko@gmail.com>
2016-06-23 00:11:37 -0400

    Initial commit of the PartnerLogin sample

---
+++ samples/debugging/Pandorum.Samples.PartnerLogin/Program.cs
@@ -0,0 +1,26 @@
+﻿using Pandorum.Options.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pandorum.Samples.PartnerLogin
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async static Task MainAsync()
+        {
+            using (var client = new PandoraClient())
+            {
+                await client.PartnerLogin();
+
+                Console.WriteLine("Succesfully logged in.");
+            }
+        }
+    }
+}
