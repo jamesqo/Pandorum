51619480a67d72b201e750753af1a35bbf45ec2e
James Ko <jamesqko@gmail.com>
2016-06-22 23:17:35 -0400

    Initial commit of the decryption logic

--- src/Pandorum.Core.Cryptography/Core/Cryptography/BlowfishEcb.cs
+++ src/Pandorum.Core.Cryptography/Core/Cryptography/BlowfishEcb.cs
@@ -14,6 +14,117 @@ namespace Pandorum.Core.Cryptography
 {
     public static class BlowfishEcb
     {
+        // Decryption
+        // TODO: Maybe separate this out into a new file, like BlowfishEcb.Decryption.cs?
+
+        public static ArrayLease<byte> DecryptBytes(byte[] ciphertext, byte[] key, out int count)
+        {
+            return DecryptBytes(
+                new ArraySegment<byte>(ciphertext),
+                new ArraySegment<byte>(key),
+                out count);
+        }
+
+        public static ArrayLease<byte> DecryptBytes(ArraySegment<byte> ciphertext, ArraySegment<byte> key, out int count)
+        {
+            // Blowfish processes things in blocks of 8, so
+            // it doesn't make sense to have a non-multiple
+            // of 8 length ciphertext (TODO: right?)
+            if ((ciphertext.Count & 7) != 0) // ciphertext.Count % 8 != 0
+            {
+                // TODO: Have a Strings.resx file for this
+                // Does project.json support that? / What benefits will that yield?
+                throw new ArgumentException(
+                    message: "The length of the ciphertext needs to be divisible by 8.",
+                    paramName: nameof(ciphertext));
+            }
+
+            // Since there's guaranteed to be no leftover,
+            // the count is just ciphertext.Count
+            count = ciphertext.Count;
+
+            var engine = new BlowfishEngine();
+            var keyParameter = new KeyParameter(key.Array, key.Offset, key.Count);
+            engine.Init(forEncryption: false, parameters: keyParameter);
+
+            var lease = ArrayPool<byte>.Shared.Lease(count);
+
+            try
+            {
+                var encryptBuffer = ciphertext.Array;
+                var decryptBuffer = lease.Array;
+
+                for (int i = 0; i < count; i += 8)
+                {
+                    engine.ProcessBlock(encryptBuffer, ciphertext.Offset, decryptBuffer, i);
+                }
+            }
+            catch
+            {
+                lease.Dispose();
+                throw;
+            }
+
+            return lease;
+        }
+
+        // TODO: Maybe public DecryptBytesToString here?
+
+        public static string DecryptHexToString(string ciphertext, string key)
+        {
+            return DecryptHexToString(ciphertext, key, Encoding.UTF8);
+        }
+
+        public static string DecryptHexToString(string ciphertext, string key, Encoding encoding)
+        {
+            // First translate the ciphertext from hex -> byte array
+            // Throw if the string isn't divisible by 2, since bytes are 2 hex digits
+            if ((ciphertext.Length & 1) != 0) // ciphertext.Length % 2 != 0
+            {
+                // TODO: Strings.resx if it's worth it (see notes above)
+                throw new ArgumentException(
+                    message: "The ciphertext needs to be hex-encoded and have a length divisible by 2.",
+                    paramName: nameof(ciphertext));
+            }
+
+            int bufferLength = ciphertext.Length / 2;
+            var rented = ArrayPool<byte>.Shared.Rent(bufferLength);
+
+            try
+            {
+                Debug.Assert(bufferLength <= rented.Length);
+
+                // Do the translation via Hexadecimal.ToByte
+                int i = 0, j = 0;
+                while (i < bufferLength)
+                {
+                    rented[i] = Hexadecimal.ToByte(ciphertext[j], ciphertext[j + 1]);
+
+                    i += 1;
+                    j += 2;
+                }
+
+                Debug.Assert(i == bufferLength && j == i * 2);
+
+                // Now call DecryptBytes with the key/encrypted buffer
+                var encryptedBytes = new ArraySegment<byte>(rented, 0, bufferLength);
+                var keyBytes = new ArraySegment<byte>(encoding.GetBytes(key)); // TODO: Pool/memoize this?
+
+                int decryptedCount;
+                using (var lease = DecryptBytes(encryptedBytes, keyBytes, out decryptedCount))
+                {
+                    return encoding.GetString(lease.Array, 0, decryptedCount);
+                }
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(rented);
+            }
+        }
+
+        // Encryption
+        // TODO: Maybe separate this out into a new file, like BlowfishEcb.Encryption.cs?
+
         public static ArrayLease<byte> EncryptBytes(byte[] plaintext, byte[] key, out int count)
         {
             return EncryptBytes(
@@ -110,7 +221,7 @@ namespace Pandorum.Core.Cryptography
             int byteCount = encoding.GetByteCount(plaintext);
             // byteCount needs to be a multiple of 8, so if it
             // isn't round up to the nearest multiple
-            byteCount = ((byteCount - 1) & ~7) + 8;
+            byteCount = (byteCount + 7) & ~7;
 
             var textBytes = ArrayPool<byte>.Shared.Rent(byteCount);
             try
