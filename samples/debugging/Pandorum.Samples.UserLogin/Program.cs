6439df37b1a94da7f895991059f22a85d1f6e6ea
James Ko <jamesqko@gmail.com>
2016-06-23 23:37:45 -0400

    Add note to use environment variables in auth tests

--- samples/debugging/Pandorum.Samples.UserLogin/Program.cs
+++ samples/debugging/Pandorum.Samples.UserLogin/Program.cs
@@ -17,6 +17,7 @@ namespace Pandorum.Samples.UserLogin
             using (var client = new PandoraClient())
             {
                 await client.PartnerLogin();
+                // TODO: Switch to using environment variables
                 await client.UserLogin("YOUR_EMAIL", "YOUR_PASSWORD"); // Please don't commit this!
             }
         }
