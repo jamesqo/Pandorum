5e9186fcbeb7fd45386dc3c3f32ea0b971e85ed6
James Ko <jamesqko@gmail.com>
2016-06-23 23:05:19 -0400

    Implement Category

--- src/Pandorum/Stations/Category.cs
+++ src/Pandorum/Stations/Category.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Pandorum.Core;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,16 @@ namespace Pandorum.Stations
 {
     public class Category
     {
+        [JsonConstructor]
+        private Category(GenreStation[] stations, string categoryName)
+        {
+            Debug.Assert(stations != null && categoryName != null);
+
+            Name = categoryName;
+            Stations = stations.AsReadOnly();
+        }
+
+        public string Name { get; }
+        public IEnumerable<GenreStation> Stations { get; }
     }
 }
