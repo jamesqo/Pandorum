95708b31d561f00bf76248c930c523e266699cc7
James Ko <jamesqko@gmail.com>
2016-06-23 22:12:31 -0400

    Add JTokenExtensions

---
+++ src/Pandorum/Core/Json/JTokenExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pandorum.Core.Json
+{
+    internal static class JTokenExtensions
+    {
+        private static IContractResolver s_camelCaseResolver;
+
+        private static IContractResolver CamelCaseResolver =>
+            s_camelCaseResolver ?? (s_camelCaseResolver = new CamelCasePropertyNamesContractResolver());
+
+        public static T CamelCaseToObject<T>(this JToken token)
+        {
+            var settings = new JsonSerializerSettings();
+            settings.ContractResolver = CamelCaseResolver;
+            var serializer = JsonSerializer.CreateDefault(settings);
+            return token.ToObject<T>(serializer);
+        }
+    }
+}
