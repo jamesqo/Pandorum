0ab13798f3e248e5f77e37cc9280779ed8228de6
James Ko <jamesqko@gmail.com>
2016-06-23 23:01:26 -0400

    Genericification

---
+++ src/Pandorum/Core/ListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pandorum.Core
+{
+    internal static class ListExtensions
+    {
+        public static ReadOnlyCollection<T> AsReadOnly<T>(this IList<T> list)
+        {
+            if (list == null)
+                throw new ArgumentNullException(nameof(list));
+
+            return new ReadOnlyCollection<T>(list);
+        }
+    }
+}
