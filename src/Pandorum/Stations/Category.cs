119442178a3c0660d1b2edb65ea8f9e5673c6cbf
James Ko <jamesqko@gmail.com>
2016-06-27 14:58:28 -0400

    Make Category impl IGrouping

--- src/Pandorum/Stations/Category.cs
+++ src/Pandorum/Stations/Category.cs
@@ -2,6 +2,7 @@
 using Pandorum.Core;
 using Pandorum.Core.DataTransfer.Stations;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -9,19 +10,24 @@ using System.Threading.Tasks;
 
 namespace Pandorum.Stations
 {
-    public class Category
+    public class Category : IGrouping<string, GenreStation>
     {
+        private readonly IEnumerable<GenreStation> _stations;
+
         internal Category(CategoryDto dto)
         {
             if (dto == null)
                 throw new ArgumentNullException(nameof(dto));
 
             Name = dto.CategoryName;
-            Stations = dto.Stations.Select(s => new GenreStation(s));
+            _stations = dto.Stations.Select(s => new GenreStation(s));
         }
 
         public string Name { get; }
-        public IEnumerable<GenreStation> Stations { get; }
+
+        string IGrouping<string, GenreStation>.Key => Name;
+        IEnumerator<GenreStation> IEnumerable<GenreStation>.GetEnumerator() => _stations.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => _stations.GetEnumerator();
 
         public override string ToString() => Name;
     }
