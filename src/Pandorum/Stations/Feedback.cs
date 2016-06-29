fda86b753784cae14c08f5c2a63ba40dda9c87dc
James Ko <jamesqko@gmail.com>
2016-06-28 23:08:05 -0400

    Implement Feedback

--- src/Pandorum/Stations/Feedback.cs
+++ src/Pandorum/Stations/Feedback.cs
@@ -12,6 +12,15 @@ namespace Pandorum.Stations
         {
             if (dto == null)
                 throw new ArgumentNullException(nameof(dto));
+
+            var thumbsUp = dto.ThumbsUp.Select(r => new Rating(r));
+            ThumbsUp = new RatingCollection(thumbsUp, dto.TotalThumbsUp);
+
+            var thumbsDown = dto.ThumbsDown.Select(r => new Rating(r));
+            ThumbsDown = new RatingCollection(thumbsDown, dto.TotalThumbsDown);
         }
+
+        public RatingCollection ThumbsUp { get; }
+        public RatingCollection ThumbsDown { get; }
     }
 }
---
+++ src/Pandorum/Stations/RatingCollection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pandorum.Stations
+{
+    public class RatingCollection : IReadOnlyCollection<Rating>
+    {
+        private readonly IEnumerable<Rating> _ratings;
+
+        public RatingCollection(IEnumerable<Rating> ratings, int count)
+        {
+            if (ratings == null)
+                throw new ArgumentNullException(nameof(ratings));
+            if (count < 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            Debug.Assert(count == ratings.Count());
+
+            _ratings = ratings;
+            Count = count;
+        }
+
+        public int Count { get; }
+
+        public IEnumerator<Rating> GetEnumerator() => _ratings.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
