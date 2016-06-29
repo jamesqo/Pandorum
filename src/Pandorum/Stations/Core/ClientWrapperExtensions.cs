dce4455ab12425cd0b19895d6bf106d13edb7a7a
James Ko <jamesqko@gmail.com>
2016-06-28 22:06:08 -0400

    DebuggerStepThrough for JsonClient() method

--- src/Pandorum/Stations/Core/ClientWrapperExtensions.cs
+++ src/Pandorum/Stations/Core/ClientWrapperExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Pandorum.Core.Net;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,12 @@ namespace Pandorum.Stations.Core
 {
     internal static class ClientWrapperExtensions
     {
+        [DebuggerStepThrough]
         public static IPandoraJsonClient JsonClient(this IClientWrapper wrapper)
         {
+            if (wrapper == null)
+                throw new ArgumentNullException(nameof(wrapper));
+
             return wrapper.InnerClient.JsonClient;
         }
     }
