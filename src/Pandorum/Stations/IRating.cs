fc093cd681f674e6f8789ab8116c248bd6e98a26
James Ko <jamesqko@gmail.com>
2016-06-28 23:01:08 -0400

    Implement Rating

---
+++ src/Pandorum/Stations/IRating.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pandorum.Stations
+{
+    public interface IRating
+    {
+        string FeedbackId { get; }
+    }
+}
--- src/Pandorum/Stations/Rating.cs
+++ src/Pandorum/Stations/Rating.cs
@@ -1,4 +1,5 @@
-﻿using Pandorum.Core.DataTransfer.Stations;
+﻿using Pandorum.Core.DataTransfer;
+using Pandorum.Core.DataTransfer.Stations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +7,35 @@ using System.Threading.Tasks;
 
 namespace Pandorum.Stations
 {
-    public class Rating
+    public class Rating : IRating
     {
+        private readonly string _feedbackId;
+
         internal Rating(RatingDto dto)
         {
+            if (dto == null)
+                throw new ArgumentNullException(nameof(dto));
+            
+            AlbumArtUrl = dto.AlbumArtUrl;
+            ArtistName = dto.ArtistName;
+            SongName = dto.SongName;
 
+            IsPositive = dto.IsPositive;
+            DateCreated = dto.DateCreated.ToDateTimeOffset();
+
+            _feedbackId = dto.FeedbackId;
         }
+
+        // TODO: Should have a Song property that implements
+        // ISongInfo and exposes ArtUrl, instead of these 3 properties,
+        // once we learn how to decrypt musicToken/songIdentity
+        public string SongName { get; }
+        public string ArtistName { get; }
+        public string AlbumArtUrl { get; }
+
+        public bool IsPositive { get; }
+        public DateTimeOffset DateCreated { get; }
+
+        string IRating.FeedbackId => _feedbackId;
     }
 }
