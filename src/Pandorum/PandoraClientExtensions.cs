115664a2a5ab33e699d97e0b468cb06b0a668a45
James Ko <jamesqko@gmail.com>
2016-06-29 20:24:37 -0400

    [experimental] Add overloads of Http{s} extension methods accepting Action/Func for scoped change of scheme

--- src/Pandorum/PandoraClientExtensions.cs
+++ src/Pandorum/PandoraClientExtensions.cs
@@ -9,13 +9,31 @@ namespace Pandorum
 {
     public static class PandoraClientExtensions
     {
+        // If/when Uri.UriSchemeHttp{s} is exposed in
+        // .NET Core, we may want to get rid of these
+        // and add a `using static System.Uri` at the top
+        private const string UriSchemeHttp = "http";
+        private const string UriSchemeHttps = "https";
+
         public static T Http<T>(this T client) where T : IPandoraClient
         {
             if (client == null)
                 throw new ArgumentNullException(nameof(client));
 
-            ChangeEndpointScheme(client.Inner.Settings, "http");
+            ChangeEndpointScheme(client.Inner.Settings, UriSchemeHttp);
             return client;
+        }
+
+        // NOTE: The APIs that accept an Action/Func are experimental (same for Https)
+
+        public static void Http<T>(this T client, Action<T> action) where T : IPandoraClient
+        {
+            RunWithScheme(client, action, UriSchemeHttp);
+        }
+
+        public static TResult Http<T, TResult>(this T client, Func<T, TResult> func) where T : IPandoraClient
+        {
+            return RunWithScheme(client, func, UriSchemeHttp);
         }
 
         public static T Https<T>(this T client) where T : IPandoraClient
@@ -23,13 +41,18 @@ namespace Pandorum
             if (client == null)
                 throw new ArgumentNullException(nameof(client));
 
-            ChangeEndpointScheme(client.Inner.Settings, "https");
+            ChangeEndpointScheme(client.Inner.Settings, UriSchemeHttps);
             return client;
         }
 
-        private static void ChangeEndpointScheme(PandoraClientSettings settings, string scheme)
+        public static void Https<T>(this T client, Action<T> action) where T : IPandoraClient
         {
-            settings.Endpoint = UriHelpers.ChangeScheme(settings.Endpoint, scheme);
+            RunWithScheme(client, action, UriSchemeHttps);
+        }
+
+        public static TResult Https<T, TResult>(this T client, Func<T, TResult> func) where T : IPandoraClient
+        {
+            return RunWithScheme(client, func, UriSchemeHttps);
         }
 
         // Convenience method
@@ -40,6 +63,60 @@ namespace Pandorum
                 throw new ArgumentNullException(nameof(client));
 
             return client.Inner.JsonClient;
+        }
+
+        private static void ChangeEndpointScheme(PandoraClientSettings settings, string scheme)
+        {
+            settings.Endpoint = UriHelpers.ChangeScheme(settings.Endpoint, scheme);
+        }
+
+        // RunWithScheme: Run the specified Action/Func on the client after changing
+        // its endpoint scheme, then set it back to what it originally was
+
+        private static void RunWithScheme<T>(T client, Action<T> action, string scheme)
+            where T : IPandoraClient
+        {
+            if (client == null)
+                throw new ArgumentNullException(nameof(client));
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            var settings = client.Inner.Settings;
+            var original = settings.Endpoint;
+
+            ChangeEndpointScheme(settings, scheme);
+
+            try
+            {
+                action(client);
+            }
+            finally
+            {
+                settings.Endpoint = original;
+            }
+        }
+
+        private static TResult RunWithScheme<T, TResult>(T client, Func<T, TResult> func, string scheme)
+            where T : IPandoraClient
+        {
+            if (client == null)
+                throw new ArgumentNullException(nameof(client));
+            if (func == null)
+                throw new ArgumentNullException(nameof(func));
+
+            var settings = client.Inner.Settings;
+            var original = settings.Endpoint;
+
+            ChangeEndpointScheme(settings, "http");
+
+            try
+            {
+                return func(client);
+            }
+            finally
+            {
+                settings.Endpoint = original;
+            }
         }
     }
 }
